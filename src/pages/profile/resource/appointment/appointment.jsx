c31b643089adf4799e451270d436ad7ae739a5ac
GyovanneMS <Gyovannecontato@gmail.com>
2023-04-26 09:24:48 -0300

    Filter City

--- src/pages/profile/resource/appointment/appointment.jsx
+++ src/pages/profile/resource/appointment/appointment.jsx
@@ -94,17 +94,17 @@ export const Appointment = () => {
     
     return (
     <section id='buttonCanceled' className={` md:w-3/4 border-4 border-[#9ED1B7] rounded-lg overflow-auto m-10 bg-white ${classButton}`}>
-        <form onSubmit={handleSubmit(submitAppointment)} className="bg-green-300 w-full">
-        <div className='p-2 md:p-20 bg-purple-500 w-full'>
-            <h1 className='flex justify-start text-3xl md:text-5xl font-semibold pt-2 md:pt-10'>Selecione o animal</h1>
-            <div className='flex justify-between md:pt-10 w-full bg-yellow-400'>
-                <div className='flex flex-col md:w-2/4 md:p-5 overflow-y-auto bg-yellow-900 h-full'>
+        <form onSubmit={handleSubmit(submitAppointment)} className=" flex justify-start w-full">
+            <div className='p-2 md:p-20  w-full'>
+                <h1 className='flex justify-start text-3xl md:text-5xl font-semibold pt-2 md:pt-10'>Selecione o animal</h1>
+                <div className='flex justify-between md:pt-10 w-full '>
+                    <div className='flex flex-col md:w-2/4 md:p-5 overflow-y-auto border-2 rounded-xl border-gray-400 h-full'>
                     {petsAll.map(pet => {
                         if (pet.photo == null || pet.photo == undefined) {
                             return(<button
+                                className='py-2 mt-2 bg-slate-500 gap-2 rounded-md flex-grow border-2 border-black flex flex-row'
                                 key={pet.id}
                                 type='button'
-                                className='py-2 gap-2 rounded-md flex-grow border-2 border-black flex flex-row'
                                 id={pet.id}
                                 onClick={() => handlePetSelection(pet)}
                                 >  
@@ -115,7 +115,7 @@ export const Appointment = () => {
                             return(<button
                                 key={pet.id}
                                 type='button'
-                                className='py-2 gap-2 rounded-md flex-grow border-2 border-black flex flex-row'
+                                className='py-2 mt-2 bg-slate-500 gap-2 rounded-md flex-grow border-2 border-black flex flex-row'
                                 id={pet.id}
                                 onClick={() => handlePetSelection(pet)}
                                 >  
@@ -125,42 +125,60 @@ export const Appointment = () => {
                         }
                         
                     })}
+                    </div>
+                    <div className='hidden md:flex justify-end pl-20'>
+                    <img className=' w-56' src={petImage}></img>
+                        <div className='flex flex-col pl-10 md:pt-10'>
+                            <label>
+                                <input disabled type="text" name="firstName" value={petsName} className='bg-transparent border-none  md:text-5xl font-semibold ' />
+                            </label>
+                            <label>
+                                <input disabled type="text" name="firstName" value={petsEspecie} className='bg-transparent border-none text-1xl md:text-6xl text-[#A9A9A9]' />
+                            </label>
+                        </div>
+                    </div>
                 </div>
-            <div className='hidden md:flex justify-end pl-20'>
-                <img className=' w-56' src={petImage}></img>
-                <div className='flex flex-col pl-10 md:pt-10'>
-                <label>
-                    <input disabled type="text" name="firstName" value={petsName} className='bg-transparent border-none  md:text-5xl font-semibold ' />
-                </label>
-                <label>
-                    <input disabled type="text" name="firstName" value={petsEspecie} className='bg-transparent border-none text-1xl md:text-6xl text-[#A9A9A9]' />
-                </label>
+            </div>
+            <div className=' w-full'>
+                <div className=' p-4'>
+                    <label htmlFor="descricao" className="block mb-2">Descrição:</label>
+                    <textarea id="descricao" name="descricao" className="block border-2 border-black w-full rounded-md focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" rows="4" onChange={newDescription}></textarea>
+                </div>
+                <div className=''>
+                    <h2 className='hidden md:flex md:text-5xl font-semibold pl-20'>Selecione a data e a hora</h2>
+                    <h2 className='flex justify-center text-3xl md:hidden md:text-5xl font-semibold'>Data e hora</h2>
+                    <div className='flex xl:flex-row flex-col justify-between lg:gap-8 gap-2 w-full  pl-14 md:pl-20 pt-3 md:pt-10'>
+                        <label className='w-full flex flex-col text-gray-400 '>
+                            Data
+                            <input type='date' onChange={newDate}min={dataFormatada}/>
+                        </label>
+                        <label className='w-full flex flex-col text-gray-400'>
+                            Hora
+                            <input type='time' onChange={newTime} />
+                        </label>
+                    </div>
+                </div>
+                <div className='p-3 md:pl-20 md:pr-20'>
+                    <div className='flex mt-2 md:mt-10 justify-between gap-5'>
+                        <button className={`p-2 md:w-56 md:text-center md:h-20 border rounded-full bg-[#F9DEDC] text-[#410E0B] font-bold text-2xl origin-center `} type="button"
+                            onClick={
+                                () => {
+                                    if (document.getElementById("buttonCanceled").classList.contains("hidden")) {
+                                        console.log("teste");
+                                    } else {
+                                        document.getElementById("buttonCanceled").classList.add("hidden")
+                                    }
+                                }
+                            }>
+                            Cancelar
+                        </button>
+                        <button type="submit" className='md:ml-56 p-2 w-32 md:w-56 text-center md:h-20 border rounded-full bg-[#9ED1B7] text-[#41564B] font-bold text-2xl'>
+                            Marcar
+                        </button>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
-        <div>
-            <label htmlFor="descricao">Descrição:</label>
-            <textarea id="descricao" name="descricao" onChange={newDescription}></textarea>
-        </div>
-        <div>
-            <h2 className='hidden md:flex md:text-5xl font-semibold pl-20'>Selecione a data e a hora</h2>
-            <h2 className='flex justify-center text-3xl md:hidden md:text-5xl font-semibold'>Data e hora</h2>
-            <div className='flex xl:flex-row flex-col justify-between lg:gap-8 gap-2 w-full pl-14 md:pl-20 pt-3 md:pt-10'>
-            <label className='w-full flex flex-col'>
-                <input type='date' onChange={newDate}min={dataFormatada}/>
-            </label>
-            <label className='w-full flex flex-col'>
-                <input type='time' onChange={newTime} />
-            </label>
-            </div>
-        </div>
-        <button type="submit" className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10">
-            Agendar
-        </button>
         </form>
     </section>
     );      
 };
-
-
