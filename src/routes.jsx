3602f1f7e3a062350c7bd350e6e5d1bd918ac185
= <=>
2023-05-09 11:14:20 -0300

    fixing code format and removing obsolete imports

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/home/Home";
 import { HeaderWeb } from "./pages/home/resource/HeaderWeb";
 import { LandingPage } from "./pages/home/LandingPage";
-import { HomeWeb } from "./pages/home/Home-Web";
-import { WebFunction} from "./pages/home/WebFunction";
-import { ChatInfos} from "./pages/home/chatInfos";
+import { HomeWeb } from "./pages/home/HomeWeb";
+import { WebFunction } from "./pages/home/WebFunction";
+import { ChatInfos } from "./pages/home/chatInfos";
 import { BlogPreview } from "./pages/home/BlogPreview";
 import { PraisedDoctors } from "./pages/home/PraisedDoctors";
 import { Assessments } from "./pages/home/Assessments";
@@ -38,44 +38,43 @@ import { BlogProfile } from "./pages/home/BlogProfile";
 export const Rotas = () => {
   return (
     <Router>
-        <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/home" element={<LandingPage />}/>
-            <Route path="/home/chatInfos" element={<ChatInfos />} />
-            <Route path="/home/WebFunction" element={<WebFunction />} />
-            <Route path="/home/BlogPreview" element={<BlogPreview />} />
-            <Route path="/home/PraisedDoctors" element={<PraisedDoctors />} />
-            <Route path="/home/Assessments" element={<Assessments/>} />
-            <Route path="/home/AppPreview" element={<AppPreview/>} />
-            <Route path="/home/Home-Web" element={<HomeWeb />} />
-            <Route path="/home/HeaderWeb" element={<HeaderWeb />}/>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />}/>
-            <Route path="/register/address" element={<RegisterAddress />} />
-            <Route path="/register/veterinary" element={<RegisterVeterinary />} />
-            <Route path="/profile/veterinary" element={<UserVet />} />
-            <Route path="/home/HeaderInfo" element={<HeaderInfo />} />
-            <Route path="/home/Footer" element={<Footer />} />
-            <Route path="/home/aboutUs" element={<AboutUs />} />
-            <Route path="/profile/upgradeUser" element={<UpgradeUser />} />
-            <Route path="/profile/infosPerson" element={<Pessoais />} />
-            <Route path="/profile/headerConfig" element={<Config />} />
-            <Route path="/profile/pet/Add" element={<PetAdd />} />
-            <Route path="/profile/pet/Config" element={<PetConfig />} />
-            <Route path="/profile/Consultas" element={<ConsultasInfo />} />
-            <Route path="/profile/editProfile" element={<EditProfile />} />
-            <Route path="/profile/editAdress" element={<AddressInfos />} />
-            <Route path="/profile/editPerson" element={<PessoaisInfos />} />
-            <Route path="/profile/editSecurity" element={<SegurancaInfo />} />
-            <Route path="/profile/editProfissionais" element={<ProfissonaisInfos />} />
-            <Route path="/profile/appointment" element={<Appointment />} />
-            <Route path="/profile/appointmentView" element={<AppointmentView />} />
-            <Route path="/profile/appointmentMenu" element={<AppointmentMenu/>} />
-            <Route path="/home/searchProfessionals" element={<SearchProfessional />} />
-            <Route path="/profile/AppointmentAsk" element={<AppointmentAsk />} />
-            <Route path="/profile/blogProfile" element={<BlogProfile />} />
-        </Routes>
-    </Router>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/home" element={<HomeWeb />} />
 
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register/address" element={<RegisterAddress />} />
+        <Route path="/register/veterinary" element={<RegisterVeterinary />} />
+
+        <Route path="/profile/veterinary" element={<UserVet />} />
+        <Route path="/home/HeaderInfo" element={<HeaderInfo />} />
+        <Route path="/home/Footer" element={<Footer />} />
+        <Route path="/home/aboutUs" element={<AboutUs />} />
+        <Route path="/profile/upgradeUser" element={<UpgradeUser />} />
+        <Route path="/profile/infosPerson" element={<Pessoais />} />
+        <Route path="/profile/headerConfig" element={<Config />} />
+        <Route path="/profile/pet/Add" element={<PetAdd />} />
+        <Route path="/profile/pet/Config" element={<PetConfig />} />
+        <Route path="/profile/Consultas" element={<ConsultasInfo />} />
+        <Route path="/profile/editProfile" element={<EditProfile />} />
+        <Route path="/profile/editAdress" element={<AddressInfos />} />
+        <Route path="/profile/editPerson" element={<PessoaisInfos />} />
+        <Route path="/profile/editSecurity" element={<SegurancaInfo />} />
+        <Route
+          path="/profile/editProfissionais"
+          element={<ProfissonaisInfos />}
+        />
+        <Route path="/profile/appointment" element={<Appointment />} />
+        <Route path="/profile/appointmentView" element={<AppointmentView />} />
+        <Route path="/profile/appointmentMenu" element={<AppointmentMenu />} />
+        <Route
+          path="/home/searchProfessionals"
+          element={<SearchProfessional />}
+        />
+        <Route path="/profile/AppointmentAsk" element={<AppointmentAsk />} />
+        <Route path="/profile/blogProfile" element={<BlogProfile />} />
+      </Routes>
+    </Router>
   );
 };
