a781a9d6fd7d43006dd67015a5fabacb1c48676f
SAMUEL RODRIGUES DE MATOS <samuelrodrigues.mt05@gmail.com>
2023-05-26 07:56:33 -0300

    ajuste no header na pagina de configuracao do perfil

--- src/pages/userProfile/resource/header.jsx
+++ src/pages/userProfile/resource/header.jsx
@@ -5,7 +5,7 @@ import {BsFillGearFill} from "react-icons/bs";
 
 export const HeaderProfile = () => {
 	return (
-		<header className="flex font-normal justify-between md:pb-5 md:h-30 text-4xl items-center md:px-12 md:pt-5">
+		<header className="flex font-normal justify-between md:pb-5 md:h-30 text-4xl items-center md:px-12 md:pt-5 relative">
 			<button id='menu-button' className="block py-3 md:px-4 mx-2 rounded focus:outline-none hover-bg-gray-200 group">
 				<span className="w-8 h-1.5 bg-[#000] mb-1 md:w-10 md:h-1.5 block"></span>
 				<span className="w-8 h-1.5 bg-[#000] mb-1 md:w-10 md:h-1.5 block"></span>
@@ -13,7 +13,7 @@ export const HeaderProfile = () => {
 			</button>
 			<SideBarMenu />
 			<Link to="/">
-				<h1 className=" text-1xl hidden xl:flex sm:flex justify-start font-bold">PetSaúde</h1>
+				<h1 className=" text-1xl hidden xl:flex sm:flex font-bold left-0 right-0 top-0 bottom-0 justify-center items-center absolute">PetSaúde</h1>
 			</Link>
 			<Link to='/profile/configuration' className="flex items-center gap-2 justify-center p-2 md:pt-2" >
 				<BsFillGearFill className='border justify-center rounded-full p-2 w-10 h-10' />
