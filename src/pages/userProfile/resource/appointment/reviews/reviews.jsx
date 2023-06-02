37dadeb32893fccff4265b40f05cf77650563bd5
Lucas Camilo <98404472+LusCamilo@users.noreply.github.com>
2023-06-01 22:08:37 -0300

    update star reviews

--- src/pages/userProfile/resource/appointment/reviews/reviews.jsx
+++ src/pages/userProfile/resource/appointment/reviews/reviews.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { ratingAdd } from "../../../../../services/integrations/rating";
 import { AiOutlineClose } from "react-icons/ai";
@@ -8,6 +8,7 @@ export const Review = (props) => {
   const { register, handleSubmit, formState: { errors }, setValue } = useForm();
   const [rating, setRating] = useState(0);
   const [description, setDescription] = useState("");
+  const [hoveredRating, setHoveredRating] = useState("")
 
   const submitForm = async () => {
     const allInfos = {
@@ -38,6 +39,14 @@ export const Review = (props) => {
     setRating(rate);
   };
 
+  const handleHover = (value) => {
+    setHoveredRating(value);
+  };
+
+  useEffect(() => {
+    console.log(rating);
+  }, [rating])
+
   return (
     <>
       <form
@@ -58,17 +67,63 @@ export const Review = (props) => {
             </p>
           </label>
         </div>
-        <div className="inline w-full pt-5 mb-7">
+        <div className="flex justify-center gap-x-3 w-full pt-5 mb-7">
+
           <input className="hidden" type="radio" id="star5" name="rating" value="1" onClick={() => handleRating(1)} />
-          <label  style={{color: 'red'}} className="cursor-pointer" htmlFor="star5" title="Excelente">&#9733;</label>
+          <label className={
+            `cursor-pointer text-6xl ${rating >= 1 ? "text-amber-400" : ""}
+            ${hoveredRating >= 1 ? 'text-amber-400' : ''}`}
+            htmlFor="star5" title="Ruim"
+            onMouseOver={() => handleHover(1)}
+            onMouseLeave={() => handleHover(0)}
+          >
+            &#9733;
+          </label>
+
           <input className="hidden" type="radio" id="star4" name="rating" value="2" onClick={() => handleRating(2)} />
-          <label className="cursor-pointer" htmlFor="star4" title="Muito bom">&#9733;</label>
+          <label className={
+            `cursor-pointer text-6xl ${rating >= 2 ? "text-amber-400" : ""}
+            ${hoveredRating >= 2 ? 'text-amber-400' : ''}`}
+            htmlFor="star4" title="Regular"
+            onMouseOver={() => handleHover(2)}
+            onMouseLeave={() => handleHover(0)}
+          >
+            &#9733;
+          </label>
+
           <input className="hidden" type="radio" id="star3" name="rating" value="3" onClick={() => handleRating(3)} />
-          <label className="cursor-pointer" htmlFor="star3" title="Bom">&#9733;</label>
+          <label className={
+            `cursor-pointer text-6xl ${rating >= 3 ? "text-amber-400" : ""}
+            ${hoveredRating >= 3 ? 'text-amber-400' : ''}`}
+            htmlFor="star3" title="Bom"
+            onMouseOver={() => handleHover(3)}
+            onMouseLeave={() => handleHover(0)}
+          >
+            &#9733;
+          </label>
+
           <input className="hidden" type="radio" id="star2" name="rating" value="4" onClick={() => handleRating(4)} />
-          <label className="cursor-pointer" htmlFor="star2" title="Regular">&#9733;</label>
+          <label className={
+            `cursor-pointer text-6xl ${rating >= 4 ? "text-amber-400" : ""}
+            ${hoveredRating >= 4 ? 'text-amber-400' : ''}`}
+            htmlFor="star2" title="Muito Bom"
+            onMouseOver={() => handleHover(4)}
+            onMouseLeave={() => handleHover(0)}
+          >
+            &#9733;
+          </label>
+
           <input className="hidden" type="radio" id="star1" name="rating" value="5" onClick={() => handleRating(5)} />
-          <label className="cursor-pointer" htmlFor="star1" title="Ruim">&#9733;</label>
+          <label className={
+            `cursor-pointer text-6xl ${rating >= 5 ? "text-amber-400" : ""}
+          ${hoveredRating >= 5 ? 'text-amber-400' : ''}`}
+            htmlFor="star1" title="Excelente"
+            onMouseOver={() => handleHover(5)}
+            onMouseLeave={() => handleHover(0)}
+          >
+            &#9733;
+          </label>
+
         </div>
 
         <div className="mb-3 w-full">
@@ -82,7 +137,7 @@ export const Review = (props) => {
             id="comment"
             name="comment"
             rows={3}
-            className="shadow appearance-none border border-black rounded w-full h-32 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pt-2"
+            className="shadow resize-none appearance-none border border-black rounded w-full h-32 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pt-2"
             onBlur={(e) => setDescription(e.target.value)}
           />
           <p className="flex justify-end text-xl text-[#A9A9A9]">
